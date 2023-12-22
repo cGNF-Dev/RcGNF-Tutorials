f9e4b594c0a485e7203f5abaf460fc43a9160660
Jesse Zhou <69114052+JesseZhou-1@users.noreply.github.com>
2023-12-22 16:18:32 -0600

    Update rcGNF_ATE.R

--- rcGNF_ATE.R
+++ rcGNF_ATE.R
@@ -2,17 +2,17 @@
 
 # Setting up environment and paths
 base_path <- '/Users/jessezhou/Desktop/cGNF_test/rcGNF/'
-folder <- 'ATE_2k'
+folder <- '_ATE'
 path <- file.path(base_path, folder, '')
-dataset_name <- 'ATE_2k'
+dataset_name <- 'ATE_20k'
 
 if (!dir.exists(path)) {
   dir.create(path, recursive = TRUE)
 }
 
 # Data Simulation
-obs <- 2000
-set.seed(1001)
+obs <- 20000
+set.seed(2813308004)
 
 A <- rbinom(n = obs, size = 1, prob = 0.5)
 Y <- ifelse(A == 1, rbinom(n = obs, size = 1, prob = 0.6), rbinom(n = obs, size = 1, prob = 0.4))
@@ -49,30 +49,29 @@ process(path = path,
         sens_corr=NULL,
         test_size = 0.2,
         cat_var = c("A", "Y"),
-        seed = 1001)
+        seed = NULL)
 
 train(path = path,
       dataset_name = dataset_name,
-      model_name = "models",
+      model_name = "20k",
       trn_batch_size = 128,
-      val_batch_size = 4096,
+      val_batch_size = 2048,
       learning_rate = 1e-4,
-      seed = 1001,
+      seed = 8675309,
       nb_epoch = 50000,
-      emb_net = c(100, 90, 80, 70, 60),
-      int_net = c(60, 50, 40, 30, 20),
+      emb_net = c(90, 80, 70, 60, 50),
+      int_net = c(50, 40, 30, 20, 10),
       nb_estop = 50,
       val_freq = 1)
 
 sim(path = path,
     dataset_name = dataset_name,
-    model_name = "models",
+    model_name = "20k",
     n_mce_samples = 10000,
     treatment = 'A',
     cat_list = c(0, 1),
     moderator = NULL,
-    quant_mod = 4,
     mediator = NULL,
     outcome = 'Y',
-    inv_datafile_name = 'rsim')
+    inv_datafile_name = 'rsim_20k')
 
